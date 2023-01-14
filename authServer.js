e9a01da0177ea0f241270938e9ade7969da7eb22
Pedro Acevedo <thepeterace@gmail.com>
2023-01-14 15:39:39 -0400

    Add Refresh Token

--- authServer.js
+++ authServer.js
@@ -19,11 +19,19 @@ db.on('error', err => {console.log(err)})
 db.once('open', () => console.log('DB Connected'))
 
 app.post('/token', async(req, res) =>{
-    const refreshToken = req.body.token
+    const refreshToken = req.body.refreshToken
     if(refreshToken == null) return res.sendStatus(403)
     const token = await Token.findOne({token: refreshToken})
-    // if(token )
-    res.send(token)
+    if(!token){
+        return res.sendStatus(403)
+    } 
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN, async(err, user) =>{
+            if(err) return res.sendStatus(403)
+            const accessToken = generateAccessToken({name: user.name})
+            const newRefreshToken = generateRefreshToken({name: user.name})
+            const find_token = await Token.findOneAndUpdate({ token: refreshToken}, {token: newRefreshToken}, {new: true})    
+            res.json({"accessToken" : accessToken, "Refresh Token": find_token.token})
+        })
 })
 
 app.post('/login', async(req, res) =>{
@@ -33,42 +41,42 @@ app.post('/login', async(req, res) =>{
     }
     try{
         if( await bcrypt.compare( req.body.password, user[0].password) ){
-
-            // const username = { username: req.body.username}
             const username = { name: req.body.username}
             const accessToken = generateAccessToken(username)
-            const refreshToken = jwt.sign(username, process.env.REFRESH_TOKEN)
-            // res.send('hello')
+            const refreshToken = generateRefreshToken(username)
             const userID = user[0]._id
             const searchToken = await Token.findOne({user: userID})
+            
             if(searchToken == null){
                 const newRefreshToken = new Token({
                     token: refreshToken,
                     user: user[0]._id
                 })
                 await newRefreshToken.save()
-                res.send('New token saved')
+                res.json({ accessToken : accessToken, refreshToken: newRefreshToken.token})
             } else {
                 let updateToken = await Token.findOneAndUpdate(
                     {user: user[0]._id}, 
                     {token: refreshToken} , 
                     {new: true}
                 )
-                res.send('Token updated')
+                // res.send('Token updated')
+                res.json({ accessToken : accessToken, refreshToken: updateToken.token})
+
             }
-            // res.json({ accessToken : accessToken, refreshToken: refreshToken})
-            // res.send(searchToken)
         } else {
             res.send('Could not login')
         }
     } catch (err) {
         res.json({ message: err.message })
-        // res.send(  user[0].password)
     }
 })
 
 const generateAccessToken = (user) => {
     return jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '20s'})
+}
+const generateRefreshToken = (user) => {
+    return jwt.sign(user, process.env.REFRESH_TOKEN)
 }
 
 app.listen(4000)
