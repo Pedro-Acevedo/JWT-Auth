71cae6122b2fca742d124f91b55a82cfa68b30a8
Pedro Acevedo <thepeterace@gmail.com>
2023-01-16 18:46:55 -0400

    Add Validation and Code Refactor

--- authServer.js
+++ authServer.js
@@ -19,63 +19,74 @@ const db = mongoose.connection
 db.on('error', err => {console.log(err)})
 db.once('open', () => console.log('DB Connected'))
 
-app.post('/login', async(req, res) =>{
-    let user = await User.find({userName: req.body.username})
-    if (user == null){
-         res.send('User not found')
-    }
-    try{
-        if( await bcrypt.compare( req.body.password, user[0].password) ){
-            const username = { name: req.body.username}
-            const accessToken = generateAccessToken(username)
-            const refreshToken = generateRefreshToken(username)
-            const userID = user[0]._id
-            const searchToken = await Token.findOne({user: userID})
-            
-            if(searchToken == null){
-                const newRefreshToken = new Token({
-                    token: refreshToken,
-                    user: user[0]._id
-                })
-                await newRefreshToken.save()
-                // Set secure: true in produccion
-                res.cookie('jwt', newRefreshToken.token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000
-                    //, secure:process.env.SECURE 
-                })
-                res.json({ accessToken : accessToken })
-            } else {
-                let updateToken = await Token.findOneAndUpdate(
-                    {user: user[0]._id}, 
-                    {token: refreshToken} , 
-                    {new: true}
-                )
-                // JWT cookie was created as httpOnly so it can't be modified with Js
-                // Set secure: true in produccion
-                res.cookie('jwt', updateToken.token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 //, secure:process.env.SECURE 
-            })
-                res.json({ accessToken : accessToken })       
-            }
-        } else {
-            res.send('Could not login')
-        }
-    } catch (err) {
-        res.json({ message: err.message })
-    }
+app.post('/login', validateEmptyFields, validateCredentials, async(req, res) =>{
+    // validateEmptyFields(req.body.username, req.body.password, res)
+    // try{
+    //     let query = User.findOne({})
+    //     query = query.regex('userName', new RegExp(req.body.username, 'i'))
+    //     const user = await query.exec()
+
+    //     if (user == null){
+    //         return res.send('no user')
+    //     }
+    //     if( req.body.username.toUpperCase() !== user.userName.toUpperCase() ){
+    //         return res.send('Username is wrong')
+    //     }    
+    //     if( await bcrypt.compare( req.body.password, user.password) ){
+    //         const username = { name: req.body.username}
+    //         const accessToken = generateAccessToken(username)
+    //         const refreshToken = generateRefreshToken(username)
+    //         const userID = user._id
+    //         const searchToken = await Token.findOne({user: userID})
+    //         if(searchToken == null){
+    //             const newRefreshToken = new Token({
+    //                 token: refreshToken,
+    //                 user: user._id
+    //             })
+    //             await newRefreshToken.save()
+    //             // Set secure: true in produccion
+    //             res.cookie('jwt', newRefreshToken.token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000
+    //                 , secure:process.env.SECURE 
+    //             })
+    //             res.json({ accessToken : accessToken })
+    //         } else {
+    //             let updateToken = await Token.findOneAndUpdate(
+    //                     {user: user._id}, 
+    //                     {token: refreshToken} , 
+    //                     {new: true}
+    //                 )
+    //                 // JWT cookie was created as httpOnly so it can't be modified with Js
+    //                 // Set secure: true in produccion
+    //                 res.cookie('jwt', updateToken.token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000,    secure:process.env.SECURE 
+    //                 })
+    //                 res.json({ user: user ,accessToken : accessToken })       
+    //             }   
+    //     } else {
+    //             res.send('Wrong Password')
+    //     }
+        
+    // } catch (err) {
+    //     res.json({ message: err.message })
+    // }
 })
 
 app.get('/token', async(req, res) =>{
     const cookie = req.cookies
     if (!cookie?.jwt) return res.sendStatus(403)
-    const token = await Token.findOne({token: cookie.jwt})
-    const users = await User.findById({ _id: token.user })
-    if(!token){
-        return res.sendStatus(403)
-    } 
+    try {
+        const token = await Token.findOne({token: cookie.jwt})
+        const users = await User.findById({ _id: token.user })
+        if(!token){
+            return res.sendStatus(403)
+        } 
         jwt.verify(cookie.jwt, process.env.REFRESH_TOKEN, async(err, user) =>{
             if(err || users.userName !== user.name) return res.sendStatus(403)
             const accessToken = generateAccessToken({name: user.name})   
             res.json({"accessToken" : accessToken})
         })  
+    } catch ( err ) {
+        res.json({ "message": err.message})
+    }
 })
 
 
@@ -95,9 +106,9 @@ app.get('/logout', async(req, res) => {
     // console.log(userJWT);
     const deletedToken = await Token.findOne({token: cookies.jwt})
             // return res.send( jwt)
-            // res.clearCookie('jwt', { httpOnly: true,    maxAge: 24 * 60 * 60 * 1000, 
+            res.clearCookie('jwt', { httpOnly: true,    maxAge: 24 * 60 * 60 * 1000 
                 // secure: process.env.SECURE
-            // })
+            })
         await deletedToken.remove()
         } catch (err) {
             res.json({ "message" : err.message})
@@ -105,11 +116,78 @@ app.get('/logout', async(req, res) => {
    
     // res.sendStatus(204)
 })
+
+// Functions
 const generateAccessToken = (user) => {
     return jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '30s'})
 }
 const generateRefreshToken = (user) => {
     return jwt.sign(user, process.env.REFRESH_TOKEN, { expiresIn: '1d'})
+}
+const generateTokenCookie = (res, token) => {
+    res.cookie('jwt', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000
+        , secure:process.env.SECURE 
+    })
+}
+
+// MiddleWare
+function validateEmptyFields(req, res, next) {
+    const { username, password } = req.body
+    try{
+        if(username == null || username == '') return res.send('User not valid')
+        if(password == null || password == '') return res.send('Password not valid')
+    } catch{
+        res.sendStatus(500)
+    }
+    next()
+}
+
+async function validateCredentials(req, res, next){
+    try{
+        let query = User.findOne({})
+        query = query.regex('userName', new RegExp(req.body.username, 'i'))
+        const user = await query.exec()
+
+        if (user == null){
+            return res.send('no user')
+        }
+        if( req.body.username.toUpperCase() !== user.userName.toUpperCase() ){
+            return res.send('Incorrect username or password')
+        }    
+        if( await bcrypt.compare( req.body.password, user.password) ){
+            const username = { name: req.body.username}
+            const accessToken = generateAccessToken(username)
+            const refreshToken = generateRefreshToken(username)
+            const userID = user._id
+            const searchToken = await Token.findOne({user: userID})
+            if(searchToken == null){
+                const newRefreshToken = new Token({
+                    token: refreshToken,
+                    user: user._id
+                })
+                await newRefreshToken.save()
+                // Set secure: true in produccion
+                generateTokenCookie(res, newRefreshToken.token)
+                res.json({ accessToken : accessToken })
+            } else {
+                let updateToken = await Token.findOneAndUpdate(
+                        {user: user._id}, 
+                        {token: refreshToken} , 
+                        {new: true}
+                    )
+                    // JWT cookie was created as httpOnly so it can't be modified with Js
+                    // Set secure: true in produccion
+                    generateTokenCookie(res, updateToken)
+                    res.json({ user: user ,accessToken : accessToken })       
+                }   
+        } else {
+                res.send('Incorrect username or password')
+        }
+        
+    } catch (err) {
+        res.json({ message: err.message })
+    }
+    next()
 }
 
 app.listen(4000)
